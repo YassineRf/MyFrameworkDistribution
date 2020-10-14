dd291c18e4bcfe1d5fa733480bd8d57ffeabed22
YassineRf <yassine.toutouh@outlook.fr>
2020-10-14 16:31:26 +0200

    dftrsf

--- Package.swift
+++ Package.swift
@@ -21,10 +21,6 @@ let package = Package(
         .target(
             name: "MyFramework"
         ),
-        .binaryTarget(
-            name: "MyFramework",
-            path: "MyFramework.xcframework"
-        ),
         .testTarget(
             name: "MyFrameworkTests",
             dependencies : ["MyFramework"]
