eb2802c971c0abf3ced2c001a2b6a6ae908974df
Yassine <44257091+bbluxe@users.noreply.github.com>
2021-03-22 15:00:00 +0100

    update : binary changed

--- Package.swift
+++ Package.swift
@@ -11,6 +11,10 @@ let package = Package(
             name: "MyFramework",
             targets: ["PressKit"])
     ],
-    dependencies: [
-        .package(url: "git@github.com:YassineRf/MyFrameworkDistribution.git", .branch("master"))
+    targets: [
+        .binaryTarget(
+            name: "PressKit",
+            url:"git@github.com:userName/MyFrameworkDistribution/releases/download/0.0.1/PressKit.xcframework.zip",
+            checksum: "edef3e2146ed0166881fcac67983292e5c7baf798ec0756a928c791f194b771e"
+            )
     ])
