b4fe64bc15220286a6faa3bc3e8386a65d876ff4
Yassine <44257091+bbluxe@users.noreply.github.com>
2021-03-22 15:10:41 +0100

    update : change to https

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PressKit",
-            url:"git@github.com:userName/MyFrameworkDistribution/releases/download/0.0.1/PressKit.xcframework.zip",
+            url:"http://github.com/YassineRf/MyFrameworkDistribution/releases/download/0.0.1/PressKit.xcframework.zip",
             checksum: "edef3e2146ed0166881fcac67983292e5c7baf798ec0756a928c791f194b771e"
             )
     ])
